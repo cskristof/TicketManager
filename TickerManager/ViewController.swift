bf36dc6b0d40879d0a8aa78d7fabdcd6520c8d93
Kristóf Csapó <csapo_kristof@4dsoft.onmicrosoft.com>
2025-04-09 13:58:38 +0200

    app init

---
+++ TickerManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TickerManager
+//
+//  Created by Csapó Kristóf on 2025. 04. 09..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
